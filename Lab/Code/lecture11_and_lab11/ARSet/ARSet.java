4a82c71a6b174e20045f0eb8abc3c87f3105cc4f
Spike81 <52449420+Spike81@users.noreply.github.com>
2021-05-10 21:36:03 +0800

    commit

---
+++ Lab/Code/lecture11_and_lab11/ARSet/ARSet.java
@@ -0,0 +1,51 @@
+import java.util.Iterator;
+
+public class ARSet<T> implements Iterable<T> {
+    private T[] items;
+    private int size;
+
+    /**
+     * Create an empty set.
+     */
+    @SuppressWarnings("unchecked")
+    public ARSet() {
+        items = (T[]) new Object[100];
+        size = 0;
+    }
+
+    /**
+     * @return the number of items in the set
+     */
+    public int size() {
+        return size;
+    }
+
+    /*
+     ***************************
+     * DO NOT MODIFY CODE ABOVE
+     ***************************
+     */
+
+
+    // Copy paste your code from Week 10 here
+	
+	
+	
+	
+	
+	
+
+    
+
+
+    // EXERCISE 11.2  EQUALS
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public boolean equals(Object that) {
+		
+		
+		return false;
+    }
+	
+}
---
+++ Lab/Code/lecture11_and_lab11/Triple/Triple.java
@@ -0,0 +1,35 @@
+public class Triple<T extends Comparable<T>, S extends Comparable<S>, U extends Comparable<U>>
+        implements Comparable<Triple<T, S, U>> {
+
+    private T first;
+    private S second;
+    private U third;
+
+    public Triple(T firstElement, S secondElement, U thirdElement) {
+        first = firstElement;
+        second = secondElement;
+        third =  thirdElement;
+    }
+
+    public T getFirst() { return first; }
+    public S getSecond() { return second; }
+    public U getThird() { return third; }
+
+
+    /*
+     ***************************
+     * DO NOT MODIFY CODE ABOVE
+     ***************************
+     */
+
+
+    // EXERCISE 14.1  COMPARETO
+
+    @Override
+    public int compareTo(Triple<T, S, U> other) {
+		
+		
+		
+    }
+
+}
